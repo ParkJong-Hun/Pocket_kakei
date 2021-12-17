8822e15f8ee2c314c179948e068ebfb3b1c3f969
ParkJong-Hun <thesn515@gmail.com>
2021-12-17 12:28:52 +0900

    2021/12/17 12:28 3番目：Commit Fragments

--- app/src/main/java/com/parkjonghun/pocket_kakei/view/fragment/WeekFragment.kt
+++ app/src/main/java/com/parkjonghun/pocket_kakei/view/fragment/WeekFragment.kt
@@ -1,6 +1,23 @@
 package com.parkjonghun.pocket_kakei.view.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.parkjonghun.pocket_kakei.databinding.FragmentWeekBinding
 
 class WeekFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = FragmentWeekBinding.inflate(inflater, container, false)
+        return view.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
