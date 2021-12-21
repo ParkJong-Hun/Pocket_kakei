f493f2f08bbed4b0a6c7944ce8bb640dcf6e21d0
ParkJong-Hun <thesn515@gmail.com>
2021-12-21 13:21:09 +0900

    2021/12/21 13:20 8番目：カレンダーのスタイル、スクロール

--- app/src/main/java/com/parkjonghun/pocket_kakei/view/fragment/MonthFragment.kt
+++ app/src/main/java/com/parkjonghun/pocket_kakei/view/fragment/MonthFragment.kt
@@ -8,6 +8,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.parkjonghun.pocket_kakei.databinding.FragmentMonthBinding
 import com.parkjonghun.pocket_kakei.view.AddActivity
+import com.parkjonghun.pocket_kakei.view.decorator.SaturdayDecorator
+import com.parkjonghun.pocket_kakei.view.decorator.SundayDecorator
+import com.prolificinteractive.materialcalendarview.CalendarDay
 
 class MonthFragment: Fragment() {
     override fun onCreateView(
@@ -16,9 +19,17 @@ class MonthFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = FragmentMonthBinding.inflate(inflater, container, false)
+        //FloatingButtonクリックしたら
         view.addButton.setOnClickListener {
             startActivity(Intent(activity, AddActivity::class.java))
         }
+        //今日を選択する
+        view.monthCalendar.selectedDate = CalendarDay.today()
+        //週末の色
+        view.monthCalendar.addDecorators(
+            SaturdayDecorator(),
+            SundayDecorator()
+        )
         return view.root
     }
 
