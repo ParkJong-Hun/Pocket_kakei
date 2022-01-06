25e8f24cdde018e94bd4def172a1acb982deaafc
ParkJong-Hun <thesn515@gmail.com>
2022-01-06 16:40:20 +0900

    2021/01/06 16:40 スクロール日刊

--- app/src/main/java/com/parkjonghun/pocket_kakei/view/fragment/DayFragment.kt
+++ app/src/main/java/com/parkjonghun/pocket_kakei/view/fragment/DayFragment.kt
@@ -1,9 +1,12 @@
 package com.parkjonghun.pocket_kakei.view.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.HorizontalScrollView
+import androidx.core.view.marginEnd
 import androidx.fragment.app.Fragment
 import com.parkjonghun.pocket_kakei.databinding.FragmentDayBinding
 
@@ -12,12 +15,18 @@ class DayFragment: Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val view = FragmentDayBinding.inflate(inflater, container, false)
+        //TODO: スクロールして日付が変わるように
+        view.dayScroll.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+            Log.d("", "$scrollX,$oldScrollX")
+        }
+
         return view.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentDayBinding.bind(view)
     }
 }
