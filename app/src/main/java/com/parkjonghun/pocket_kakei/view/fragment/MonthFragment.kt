edd0138cf73c04bbfe7addc63f82d11c97c569df
ParkJong-Hun <thesn515@gmail.com>
2022-01-25 10:12:29 +0900

    2022/01/25 10:12 バグ修正“

--- app/src/main/java/com/parkjonghun/pocket_kakei/view/fragment/MonthFragment.kt
+++ app/src/main/java/com/parkjonghun/pocket_kakei/view/fragment/MonthFragment.kt
@@ -4,13 +4,11 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -30,8 +28,8 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+@SuppressLint("UseCompatLoadingForDrawables", "SetTextI18n")
 class MonthFragment: Fragment() {
-    @SuppressLint("UseCompatLoadingForDrawables", "SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
